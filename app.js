a581af64816c5fef8b1b33b0c0cc39cd19abccb2
langpineiro <langpineiro@gmail.com>
2022-10-11 09:36:16 -0600

    agregue validador

--- app.js
+++ app.js
@@ -28,27 +28,44 @@ function resetForm(){
    document.getElementById('cadena').reset();
    document.getElementById('resultado').innerHTML = "";
 }
-    
+function comparador(input){
+   let aprobacion = false;
+   for(let i = 0; i<input.length; i++){
+      if(input[i] != 'a' && input[i] != 'b'){
+            aprobacion = false;
+      }else{
+         aprobacion = true; 
+      }
+   }
+   return aprobacion;
+}
 //EL DOM OBTENIENDO LA CADENA DEL DOM
 document.getElementById('cadena').addEventListener('submit', function(event){
    const cadena = document.getElementById('caracteres').value; 
-   let estadoRetorno = validador(cadena);
-   switch(estadoRetorno){
-      case 0:
-        document.getElementById('resultado').innerHTML = "la cadena " + cadena + " No es aceptada por el automata, ha llegado al estado: " + estadoRetorno; 
-        break;
-      case 1:
-        document.getElementById('resultado').innerHTML = "la cadena " + cadena + " No es aceptada por el automata, ha llegado al estado: " + estadoRetorno; 
-        break; 
-      case 2:
-        document.getElementById('resultado').innerHTML = "la cadena " + cadena + " No es aceptada por el automata, ha llegado al estado: " + estadoRetorno; 
-        break;
-      case 3:
-        document.getElementById('resultado').innerHTML = "la cadena " + cadena + " Sí es aceptada por el automata, ha llegado al estado de aceptacion : " + estadoRetorno; 
-        break; 
-        default:
-            document.getElementById('resultado').innerHTML = "la cadena " + cadena + " No es aceptada por el automata, ha llegado al estado de error ";
-            break; 
+   
+   let validarLaCadena = comparador(cadena); 
+   if(validarLaCadena == true){
+      let estadoRetorno = validador(cadena);
+      switch(estadoRetorno){
+         case 0:
+           document.getElementById('resultado').innerHTML = "la cadena " + cadena + " No es aceptada por el automata, ha llegado al estado: " + estadoRetorno; 
+           break;
+         case 1:
+           document.getElementById('resultado').innerHTML = "la cadena " + cadena + " No es aceptada por el automata, ha llegado al estado: " + estadoRetorno; 
+           break; 
+         case 2:
+           document.getElementById('resultado').innerHTML = "la cadena " + cadena + " No es aceptada por el automata, ha llegado al estado: " + estadoRetorno; 
+           break;
+         case 3:
+           document.getElementById('resultado').innerHTML = "la cadena " + cadena + " Sí es aceptada por el automata, ha llegado al estado de aceptacion : " + estadoRetorno; 
+           break; 
+           default:
+               document.getElementById('resultado').innerHTML = "la cadena " + cadena + " No es aceptada por el automata, ha llegado al estado de error ";
+               break; 
+      }
+   }else{
+      alert("La cadena ingresada contiene otros caracteres solo debe contener a y b");
    }
+   
    event.preventDefault();
 });
